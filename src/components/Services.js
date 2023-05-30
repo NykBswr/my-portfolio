149f90688515dfbc530685092cbbbc4fca8dff9a
Nayaka Baswara <nayakabaswara@yahoo.com>
2023-05-30 17:20:33 +0700

    Services Updated

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,22 +6,27 @@ import { fadeIn } from '../variants';
 const services = [
   {
     name: 'Surat Menyurat FTMM',
-    description: 'Lorem',
+    description: "Our team is creating a user-friendly letter-writing website for Airlangga University Advanced Technology and Multidiscipline Faculty. Using Laravel, we aim to streamline the process of creating, revising, and sending letters with an intuitive interface for users of varying technical backgrounds. Our expertise will deliver a high-quality website tailored to the faculty community's specific needs.",
     link: 'Learn more',
   },
   {
     name: 'Sunrise Jeep Tour',
-    description: 'Lorem',
+    description: "We developed a React.js website for Sunrise Jeep Tour, a family-owned business offering guided Jeep tours through Mount Batur in Bali. Our user-friendly website provides comprehensive tour information, highlighting the adventure's essence and inspiring eco-conscious exploration of Bali's natural wonders.",
     link: 'Learn more',
   },
   {
     name: 'Stroke Prediction and Care',
-    description: 'Lorem',
+    description: "Our final project, a stroke prediction API accessible via our website, achieves 81% accuracy using a trained model. Medical professionals can input patient data to aid in early detection and decision-making, improving patient care. The system seamlessly integrates the model as a pickle file for prompt and appropriate medical attention.",
     link: 'Learn more',
   },
   {
     name: 'Analisis Kebahagiaan Negara di Dunia',
-    description: 'Lorem',
+    description: "Using Python and SPSS, we conducted nonparametric analysis on the 2019 World Happiness Report, demonstrating my proficiency in statistical analysis.",
+    link: 'Learn more',
+  },
+  {
+    name: 'Analisis Kebahagiaan Negara di Dunia',
+    description: "Using R Shiny, we analyzed factors influencing Gojek app expenditure among Indonesian students. Our findings reveal strong correlations with UKT and residence types, offering valuable insights for enhancing services.",
     link: 'Learn more',
   },
 ];
